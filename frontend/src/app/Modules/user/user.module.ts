ea86b3fbc3f34864aa0400b0a3cdff4005a99617
Judy Wambui <wambuijuddie065@gmail.com>
2022-09-30 22:47:56 +0300

    fixed some icons

--- frontend/src/app/Modules/user/user.module.ts
+++ frontend/src/app/Modules/user/user.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AccountInfoComponent } from './account-info/account-info.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { AccountInfoComponent } from './account-info/account-info.component';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    MatIconModule
   ]
 })
 export class UserModule { }
